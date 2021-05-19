5c48c224db38709962c3193d56a1e466b3620f40
Ryan Dennis <rrdennis@gmail.com>
2021-05-19 10:25:29 -0700

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [todos, setTodos] = useState('');
+  const [newTodo, setNewTodo] = useState('');
+
+  const getTodos = () => {
+    let todos = [];
+  
+    for (let i = 4; i <= localStorage.length; i++) {
+      let todo = JSON.parse(localStorage.getItem(i));
+      todos.push(todo);
+    }
+  
+    return todos;
+  };
+
+  
+  const onNewTodoChanged = e => setNewTodo(e.target.value);
+  
+  const onAddToListClicked = () => {
+    if (newTodo) {
+      localStorage.setItem(
+        (localStorage.length + 1).toString(), 
+        JSON.stringify({
+          todo: newTodo,
+          done: false
+        })
+        );
+        setNewTodo('');
+        return true;
+      } else {
+        return false;
+      }
+    };
+    
+  useEffect(() => {
+    let todos = getTodos();
+    setTodos(todos)
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <h1>My To Do List</h1>
+        <input value={newTodo} onChange={onNewTodoChanged} />
+        <button onClick={onAddToListClicked}>Add To List</button>
       </header>
+      <main>
+        <ul>
+          {todos.map((t, i) => 
+            <li key={i}>{t.todo} ... {t.done.toString()}</li>
+          )}
+        </ul>
+      </main>
     </div>
   );
 }
